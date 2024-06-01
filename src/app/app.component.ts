aa0c36f85eb58489dead934decb053000223c0b7
AngelRC2 <angel.condori@gmail.com>
2024-05-31 20:41:55 -0500

    menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CategoryListComponent } from './domains/notice/category-list/category-list.component';
+import { HeaderComponent } from './domains/shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule,CategoryListComponent],
+  imports: [RouterOutlet,CommonModule,CategoryListComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
